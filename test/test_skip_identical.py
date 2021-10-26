dd67ffed4d0468ae4aba8c5b086bf2dfb8e6dfe9
Laura <32573962+ErdaradunGaztea@users.noreply.github.com>
2021-10-26 16:44:15 +0200

    test sequence trimming

--- test/test_skip_identical.py
+++ test/test_skip_identical.py
@@ -1,4 +1,4 @@
-from src import build_matrix
+from src import build_matrix, trim_sequences
 
 default_config = {
     "same_award": 2,
@@ -9,6 +9,7 @@ default_config = {
 }
 seq_1 = "KYNPMCILNYRNECETCDIYLRTINLWWGKDEMEKH"
 seq_2 = "MEINDMHDIWRLQLFHWCTHTFIFFWRRINNCLLTL"
+seq_3 = "KYNPMCILNWRLQLFHWCTHTFIFFWRWGKDEMEKH"
 
 
 def test_all_equal():
@@ -17,3 +18,21 @@ def test_all_equal():
     """
     matrix = build_matrix(seq_1, seq_1, default_config)
     assert matrix is None
+
+
+def test_trimmed_sequences():
+    """
+    Identical pre- and suffixes are removed.
+    """
+    seq_1_trimmed, seq_3_trimmed = trim_sequences(seq_1, seq_3)
+    assert seq_1_trimmed == "YRNECETCDIYLRTINLW"
+    assert seq_3_trimmed == "WRLQLFHWCTHTFIFFWR"
+
+
+def test_no_trim_sequences():
+    """
+    Sequences can remain not trimmed when first and last bases are different.
+    """
+    seq_1_trimmed, seq_2_trimmed = trim_sequences(seq_1, seq_2)
+    assert seq_1_trimmed == seq_1
+    assert seq_2_trimmed == seq_2
